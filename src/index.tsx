5135076e71536c6957d5dd114b1768ca117479ae
leihan <han.lei@mail.hypers.com>
2020-07-09 18:20:57 +0800

    feat: 🎸 support ali

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { EChartOption } from 'echarts'
 import DefaultChart from './index.web'
-import WxChart from './index.wechat'
-import AliChart from './index.ali'
+import Canvas2DChart from './Canvas2DChart'
+import CanvasContextChart from './CanvasContextChart'
 import React from 'react'
 
 export interface RemaxChartProps {
@@ -13,11 +13,11 @@ const RemaxChart = (props: RemaxChartProps) => {
   const platform = process.env.REMAX_PLATFORM
 
   if (platform === 'wechat') {
-    return <WxChart {...props} />
+    return <Canvas2DChart {...props} />
   }
 
   if (platform === 'toutiao' || platform === 'ali') {
-    return <AliChart {...props} />
+    return <CanvasContextChart {...props} />
   }
 
   return <DefaultChart {...props} />
---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+export const getPlatform = () => {
+  const platform = process.env.REMAX_PLATFORM
+
+  return {
+    isWeChat: platform === 'wechat',
+    isAli: platform === 'ali',
+    isTouTiao: platform === 'toutiao',
+  }
+}
+
+export const getMiniAppApiObj = () => {
+  const { isAli, isTouTiao, isWeChat } = getPlatform()
+
+  switch (true) {
+    case isWeChat:
+      return wx
+    case isAli:
+      return my
+    case isTouTiao:
+      return tt
+    default:
+      return undefined
+  }
+}
