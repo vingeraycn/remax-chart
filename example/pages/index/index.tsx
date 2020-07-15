41675d202ed618e2f8a67b5fa3c396277250f0dd
leihan <han.lei@mail.hypers.com>
2020-07-15 18:10:14 +0800

    refactor: 💡 remove config file, clean code

--- example/pages/index/index.tsx
+++ example/pages/index/index.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react'
 import { View } from 'remax/one'
 import RemaxChart from '../../../src/index'
-import { MOCK_OPTION } from '../../../src/config'
+
+const MOCK_OPTION = {
+  tooltip: {
+    show: true,
+    confine: true,
+    renderMode: 'richText',
+    triggerOn: 'mousemove',
+  },
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: 'line',
+    },
+  ],
+}
 
 const Index = (): JSX.Element => {
   return (
